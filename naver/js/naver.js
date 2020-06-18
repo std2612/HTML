73af610b0bd79b815e3c4387338b8cd18adb3d56
Gilson <dudrb8922@gmail.com>
2020-06-18 16:20:30 +0900

    40일차 학습 예제

--- naver/js/naver.js
+++ naver/js/naver.js
@@ -1,3 +1,46 @@
 $(function(){
+    $('html').scrollTop(0);
 
+    $('.bn-btn-close').click(function(){
+        $('.box-whale').addClass('display-none');
+    });
+    var str='접기';
+    $('.btn-more').click(function(){
+        $('.box-container-menu').toggleClass('ly-open');
+        $('.btn-more').text(str);
+        if(str=='접기'){
+            str='더보기';
+        }else{
+            str='접기';
+        }
+    });
+
+    $('.btn-set').click(function(){
+        $('.btn-service-all').css('display','none');
+        $('.btn-set').css('display','none');
+        $('.btn-reset').css('display','');
+        $('.btn-save').css('display','');
+    });
+
+    $('.btn-arw').click(function(){
+        $('.btn-arw').toggleClass('fold');
+    });
+
+    var str2='block';
+    $('.btn-arw').click(function(){
+        $('.api-atcmp-wrap').css('display',str2);
+        $('.reatcmp').css('display',str2);
+        if(str2=='block'){
+            str2='none';
+        }else{
+            str2='block';
+        }
+    });
+
+    var str3='inline';
+    $('.funoff').hover(function(){
+        $('.atcmp-helper').css('display','inline');
+    }, function(){
+        $('.atcmp-helper').css('display','none');
+    });
 });
